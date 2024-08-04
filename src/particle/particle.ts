444e14e7a4d42614f4ab097403ccad8ec88ae39a
Francis Osei Annin <francisannin5@gmail.com>
2024-08-04 18:48:55 +0000

    Add: Add new updates

---
+++ src/particle/particle.ts
@@ -0,0 +1,12 @@
+
+// A particle is localised object characterised by its mass, position or charge.
+export class Particle{
+    mass: number;
+    speed: number;
+
+    constructor(mass: number,  speed: number){
+        this.mass = mass
+        this.speed = speed
+    }
+
+}
